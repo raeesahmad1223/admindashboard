be7bb2bd440dc4eafea16436277818296ee511a6
Rana Raees Ahmad Khan <common.use1223@gmail.com>
2023-04-02 18:11:45 +0500

    Creaing all dymmy components and implementing sidebar componenet just ui and Completed Soon...

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,40 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  ColorPicker,
+  Customers,
+  Editor,
+  Employees,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorMapping,
+  Stacked,
+  Pyramid,
+} from "./pages";
+
 import "./App.css";
 
 const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1" }}>
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-grey text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
+                className="text-3xl p-3 hover:drop-shadow-3xl hover:bg-light-gray text-white"
+                style={{ background: "dodgerBlue", borderRadius: "50%" }}
               >
                 <FiSettings />
               </button>
@@ -24,10 +44,12 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              SideBar
+              <Sidebar />
             </div>
           ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
           )}
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
@@ -35,37 +57,36 @@ const App = () => {
             }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              NavBar
+              <Navbar />
             </div>
           </div>
 
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path="/" element="ECommerce" />
-              <Route path="/ecommerce" element="ECommerce" />
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
 
               {/* Pages */}
-              <Route path="/orders" element="orders" />
-              <Route path="/employees" element="employees" />
-              <Route path="/customers" element="customers" />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
 
               {/* Apps */}
-              <Route path="/kanban" element="Kanban" />
-              <Route path="/editor" element="Editor" />
-              <Route path="/calendar" element="Calendar" />
-              <Route path="/color-picker" element="ColorPicker" />
-
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
 
               {/* Charts */}
-              <Route path="/line"  element="Line" />
-              <Route path="/area"  element="Area" />
-              <Route path="/bar"  element="Bar" />
-              <Route path="/pie"  element="Pie" />
-              <Route path="/financial"  element="Financial" />
-              <Route path="/color-mapping"  element="ColorMapping" />
-              <Route path="/pyramid"  element="Pyramid" />
-              <Route path="/stacked"  element="Stacked" />
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
             </Routes>
           </div>
         </div>
---
+++ src/components/ThemeSettings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThemSettings = () => {
+  return (
+    <div>ThemSettings</div>
+  )
+}
+
+export default ThemSettings
